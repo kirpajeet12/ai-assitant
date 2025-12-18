4aa186a6dc03da25386dc15194eb9ad24379f13d
kirpajeet12 <kirpajeetg@gmail.com>
2025-12-18 10:44:23 -0800

    Add static file serving from public directory

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,9 @@ import express from "express";
 import cors from "cors";
 import twilio from "twilio";
 import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 /*app.use(express.static(path.join(process.cwd(), "public")));*/
 
 
@@ -14,6 +17,8 @@ const app = express();
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "../public")));
+
 
 /* =========================
    OPENAI (LAZY INIT)
